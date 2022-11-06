49f3ef269b60f54563d11df3fde269584fd0d70d
kanpurin <kanpurin2@gmail.com>
2022-11-06 10:09:17 +0900

    [update]

--- digitDP/ADFA/intersection_adfa.hpp
+++ digitDP/ADFA/intersection_adfa.hpp
@@ -4,29 +4,28 @@
 #include <unordered_map>
 #include "digitDP/automaton.hpp"
 
-Automaton IntersectionADFA(const Automaton &adfa, const Automaton dfa) {
+Automaton IntersectionADFA(const Automaton &adfa, const Automaton &dfa) {
     assert(adfa.alphabet_size == dfa.alphabet_size);
-    assert(adfa.init == 0);
-    std::vector<std::vector<std::tuple<int,int,int>>> v(adfa.size());
     Automaton M;
-    std::unordered_map<long long,int> mp;
-    v[0].emplace_back(-1,dfa.init,-1);
-    for (int i = 0; i < adfa.size(); i++) {
-        for (int j = 0; j < v[i].size(); j++) {
-            auto [n_s,dfa_s,c_] = v[i][j];
-            if (mp.find((long long)dfa_s*adfa.size()+i) == mp.end()) {
-                mp[(long long)dfa_s*adfa.size()+i] = M.delta.size();
-                M.delta.emplace_back(std::vector<int>(adfa.alphabet_size));
-                M.is_accept.emplace_back(adfa.accept(i) && dfa.accept(dfa_s));
-                for (int c = 0; c < adfa.alphabet_size; c++) {
-                    v[adfa.next(i,c)].emplace_back(mp[(long long)dfa_s*adfa.size()+i],dfa.next(dfa_s,c),c);
-                }
+    M.alphabet_size = adfa.alphabet_size;
+    unordered_map<ll,int> table;
+    std::vector<int> x = {adfa.init}, y = {dfa.init};
+    table[x[0]*M.alphabet_size+y[0]] = 0;
+    M.init = 0;
+    for (int i = 0; i < x.size(); ++i) {
+        M.delta.emplace_back(vector<int>(M.alphabet_size, -1));
+        M.is_accept.emplace_back(adfa.accept(x[i]) && dfa.accept(y[i]));
+        for (int c = 0; c < adfa.alphabet_size; c++) {
+            int u = adfa.next(x[i],c), v = dfa.next(y[i],c);
+            ll ps = (ll)u*M.alphabet_size+v;
+            if (table.find(ps) == table.end()) {
+                table[ps] = x.size();
+                x.emplace_back(u);
+                y.emplace_back(v);
             }
-            if (n_s != -1) M.delta[n_s][c_] = mp[(long long)dfa_s*adfa.size()+i];
+            M.delta[i][c] = table[ps];
         }
     }
-    M.alphabet_size = adfa.alphabet_size;
-    M.init = 0;
     M.qsize = M.delta.size();
     return M;
 }
